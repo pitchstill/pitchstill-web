888cb982b767b2bc0d92242056b4bdac95ddd7d6
Shreyash Agarwal <shreyashag@users.noreply.github.com>
2026-03-31 21:14:40 +0530

    Update public support email

--- assets/config.js
+++ assets/config.js
@@ -2,6 +2,6 @@ window.PITCHSTILL_SITE_CONFIG = {
   gaMeasurementId: "G-HCTMTZ06YF",
   appStoreUrl: "https://apps.apple.com/in/app/pitchstill-note-training/id6759975330",
   redeemUrl: "https://apps.apple.com/redeem?ctx=offercodes&id=6759975330&code=FX4LLNRLWFPAY7HRRH",
-  supportEmail: "support@pitchstill.com",
+  supportEmail: "pitchstill@gmail.com",
   canonicalDomain: "https://pitchstill.com"
 };
